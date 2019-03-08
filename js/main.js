912d1f8a694d4ae9af6f0d2fad50f15b33de3c73
Md. Sabbir Ahamed <sabbiryan@gmail.com>
2019-03-08 17:10:37 +0600

    javascript auto advanced funaction added

--- js/main.js
+++ js/main.js
@@ -7,12 +7,20 @@ function handelPresentationClick(e) {
     var current = document.querySelector('hp-slide.active');
     var next = current.nextElementSibling;
 
-    while(next && next.tagName != "HP-SLIDE"){
+    while (next && next.tagName != "HP-SLIDE") {
         next.nextElementSibling;
     }
 
     if (next) {
         current.classList.remove("active");
         next.classList.add("active");
+
+        var aa = parseInt(next.getAttribute('data-autoadfvance'));
+
+        if (!isNaN(aa)) {
+            setTimeout(function (e) {
+                handelPresentationClick(e);
+            }, aa);
+        }
     }
 }
