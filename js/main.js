e3296b2cc7b8cad4822036562fc5cc885a52126c
Md. Sabbir Ahamed <sabbiryan@gmail.com>
2019-03-08 16:44:38 +0600

    added a javascript event handler

---
+++ js/main.js
@@ -0,0 +1,7 @@
+window.addEventListener("DOMContentLoaded", function(e){
+    document.querySelector("hp-presentation").onclick = handelPresentationClick;
+});
+
+function handelPresentationClick(e){
+    alert("clicked");
+}
